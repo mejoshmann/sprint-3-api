f1d10362c2e8c63cd16175c2556b4f9b1e87144e
Joshua Manncleargit config --global user.email mann.photography@gmail.com <mann.photography@gmail.com>
2023-05-30 15:39:13 -0700

    Fix server

--- routes/videos.js
+++ routes/videos.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/', (req, res) => {
+
+
     fs.readFile('./data/videos.json', 'utf-8', (err, data) => {
         if (err) {
-            return res.send('Warining Error');
+            return res.send('Warning Error');
         } else {
             res.json(JSON.parse(data));
         }
     });
 });
+
+module.exports = router;
